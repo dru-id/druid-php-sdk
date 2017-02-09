b637b7e6b12b911dcb77848511d2884a6f254c7c
German Jimenez <german.jc@gmail.com>
2017-02-09 16:10:39 +0100

    El calcula de rango de edad se realizaba incorrectamente al ser mujer.

--- src/Opi.php
+++ src/Opi.php
@@ -51,41 +51,55 @@ class Opi
         $opi_gender = false;
 
         try {
-            $birthday = isset($info->user->user_data->birthday) ? $info->user->user_data->birthday->value : null;
-
-            if($birthday != null){
-            	$birthday = explode("/", $birthday);
-
-            	$age = (date("md", date("U", mktime(0, 0, 0, $birthday[2], $birthday[1], $birthday[0]))) > date("md")
-                	? ((date("Y") - $birthday[2]) - 1)
-                	: (date("Y") - $birthday[2]));
-            	if (18 <= $age && $age <= 24) {
-                	$opi_age = 1;
-            	} else if (25 <= $age && $age <= 34) {
-                	$opi_age = 2;
-            	} if (35 <= $age && $age <= 44) {
-                	$opi_age = 3;
-            	} if (45 <= $age && $age <= 64) {
-                	$opi_age = 4;
-            	}
-            }
-        } catch (Exception $e) {}
-
-        try {
             $gender = isset($info->user->user_data->gender) ? $info->user->user_data->gender->vid : null;
-            if ($gender == 1) {
+            if ($gender == 1) { // Female
                 $opi_gender = 2;
-            } else if ($gender == 2) {
+            } else if ($gender == 2) { //MAle
                 $opi_gender = 1;
             }
         } catch (Exception $e) {}
 
-        if ($opi_age) {
-            $params["carry_edad"] =  $opi_age;
-        }
-
         if ($opi_gender) {
             $params["carry_sexo"] =  $opi_gender;
+
+            try {
+                $birthday = isset($info->user->user_data->birthday) ? $info->user->user_data->birthday->value : null;
+
+                if($birthday != null){
+                    $birthday = explode("/", $birthday);
+
+                    $age = (date("md", date("U", mktime(0, 0, 0, $birthday[2], $birthday[1], $birthday[0]))) > date("md")
+                        ? ((date("Y") - $birthday[2]) - 1)
+                        : (date("Y") - $birthday[2]));
+
+                    switch ($opi_gender) {
+                        case 1: //Male
+                            if (18 <= $age && $age <= 24) {
+                                $opi_age = 1;
+                            } else if (25 <= $age && $age <= 34) {
+                                $opi_age = 2;
+                            } elseif (35 <= $age && $age <= 44) {
+                            $opi_age = 3;
+                            } elseif (45 <= $age && $age <= 64) {
+                                $opi_age = 4;
+                            }
+                            break;
+                        case 2: //Female
+                            if (18 <= $age && $age <= 24) {
+                                $opi_age = 1;
+                            } else if (25 <= $age && $age <= 34) {
+                                $opi_age = 2;
+                            } elseif (35 <= $age && $age <= 64) {
+                                $opi_age = 3;
+                            }
+                            break;
+                    }
+                }
+            } catch (Exception $e) {}
+
+            if ($opi_age) {
+                $params["carry_edad"] =  $opi_age;
+            }
         }
 
         $query = array();
